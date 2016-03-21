be660d05e12ebe7c3dc63a75f4f56bdff5ad6986
Richard Cook <rcook@rcook.org>
2016-03-20 17:57:58 -0700

    Move all Template Haskell into Templates.hs

--- src/TemplateGen/DoIt.hs
+++ src/TemplateGen/DoIt.hs
@@ -6,17 +6,30 @@ module TemplateGen.DoIt (
 ) where
 
 import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text as DT
 import qualified TemplateGen.Css as C
 import qualified TemplateGen.Hash as H
-import           TemplateGen.Html
 import qualified TemplateGen.PageContext as PC
 import           TemplateGen.Resource
 import qualified TemplateGen.Settings as S
-import           TemplateGen.SiteInfo
+import qualified TemplateGen.SiteInfo as SI
 import qualified TemplateGen.TemplateContext as TC
+import qualified TemplateGen.Templates as T
+import qualified TemplateGen.Url as U
 import qualified TemplateGen.UrlString as US
 import           Text.Blaze.Html.Renderer.String (renderHtml)
 import           Yesod.Static
+
+-- A note about URLs as handled by the site generator
+-- Internal relative URLs
+-- Example: "/about" is converted to "/<root>/about"
+-- Internal absolute URLs
+-- Example: "//about" is converted to "/about"
+-- External absolute URLs
+-- Example: "http://foo/bar" is left as is
+renderHtmlUrl :: Show a => SI.SiteInfo -> U.Url -> a -> DT.Text
+renderHtmlUrl _ U.AboutR _ = "//about"    -- an internal absolute URL
+renderHtmlUrl _ U.HomeR _ = "//"          -- an internal absolute URL
 
 readResources :: FilePath -> [FilePath] -> IO [Resource]
 readResources dir = mapM $ \x ->
@@ -36,8 +49,8 @@ readResources dir = mapM $ \x ->
 localStylesheetFileNames :: [FilePath]
 localStylesheetFileNames = ["bootstrap.css", "haskell.font.css"]
 
-externalScriptUrls :: [US.UrlString]
 externalScriptUrls = ["https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"]
+externalScriptUrls :: [US.UrlString]
 
 localScriptFileNames :: [FilePath]
 localScriptFileNames = ["bootstrap.min.js", "ie10-viewport-bug-workaround.js"]
@@ -49,13 +62,13 @@ getAutogenCssResource = do
     let (_, hash) = C.generateDefaultCss
     return $ Resource ("//static/tmp/autogen-" ++ H.toString hash ++ ".css") Nothing
 
-generateHtmlTemplateFile :: SiteInfo -> IO ()
+generateHtmlTemplateFile :: SI.SiteInfo -> IO ()
 generateHtmlTemplateFile si = do
     let
         settings = S.Settings "2016" "Seattle Area Haskell Users' Group" Nothing
         pageCtx = PC.mkPageContext { PC.title = "SeaHUG - $title$" }
         templateCtx = TC.mkTemplateContext settings pageCtx
-        html = renderHtmlTemplate si templateCtx (renderHtmlUrl si)
+        html = T.renderHtmlTemplate si templateCtx (renderHtmlUrl si)
     putStrLn (renderHtml html)
 
 doIt :: IO ()
@@ -69,5 +82,5 @@ doIt = do
 
     let
         stylesheets = localStylesheets ++ [commonCssResource]
-        siteInfo = SiteInfo stylesheets scripts
+        siteInfo = SI.SiteInfo stylesheets scripts
     generateHtmlTemplateFile siteInfo
--- src/TemplateGen/Templates.hs
+++ src/TemplateGen/Templates.hs
@@ -1,10 +1,19 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module TemplateGen.Templates (
     defaultCssTemplate
+  , renderHtmlTemplate
 ) where
 
+import qualified TemplateGen.Resource as R
+import qualified TemplateGen.SiteInfo as SI
+import qualified TemplateGen.TemplateContext as TC
+import qualified TemplateGen.Url as U
+import           TemplateGen.Url (Url(AboutR, HomeR)) -- unqualified import for use in Template Haskell
+import qualified Text.Hamlet as H
 import qualified Text.Lucius as L
 
 #define CSS_TEMPLATE_PATH (TEMPLATE_DIR ++ "/default-layout.lucius")
@@ -14,3 +23,16 @@ defaultCssTemplate = $(L.luciusFile CSS_TEMPLATE_PATH)
 #else
 defaultCssTemplate = $(L.luciusFileDebug CSS_TEMPLATE_PATH)
 #endif
+
+#define LAYOUT_TEMPLATE_PATH (TEMPLATE_DIR ++ "/default-layout-wrapper.hamlet")
+renderHtmlTemplate :: SI.SiteInfo -> TC.TemplateContext -> H.HtmlUrl U.Url
+renderHtmlTemplate si tc =
+    let
+        TC.TemplateContext {..} = tc
+        pageBody _ = [H.hamlet|\$body$
+$forall r <- SI.scripts si
+    <script src=#{R.fullUrl r}>|]
+        pageHead _ = [H.hamlet|$forall r <- SI.stylesheets si
+    <link rel="stylesheet" href=#{R.fullUrl r}>|]
+    in
+        $(H.hamletFile LAYOUT_TEMPLATE_PATH)
